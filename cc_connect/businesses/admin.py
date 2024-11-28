af3a8e608eda88be11243223fdadef2e15904d52
wedberry <wberry@rollins.edu>
2024-11-28 09:29:51 -0600

    Add Business to db, admin

--- cc_connect/businesses/admin.py
+++ cc_connect/businesses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Business
+
+@admin.register(Business)
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'business_type', 'address', 'phone_number', 'website', 'latitude', 'longitude')
+    search_fields = ('name', 'country', 'business type', 'address', 'phone_number', 'website')
+    list_filter = ('latitude', 'longitude', 'country', 'business_type')
