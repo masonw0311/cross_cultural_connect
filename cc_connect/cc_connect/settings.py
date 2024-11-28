0025c7e64328091d8935b6f8793018604a6e01ef
Mason Wilson <masonw0311@gmail.com>
2024-11-28 16:18:40 -0500

    no .env in .gitignore

--- cc_connect/cc_connect/settings.py
+++ cc_connect/cc_connect/settings.py
@@ -130,7 +130,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-<<<<<<< HEAD
 
 from dotenv import load_dotenv
 import os
@@ -140,12 +139,10 @@ load_dotenv()
 
 # Use the environment variable in your settings
 GOOGLE_TRANSLATE_API_KEY = os.getenv("GOOGLE_TRANSLATE_API_KEY")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-=======
+os.environ["igin main GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 CSRF_COOKIE_SAMESITE = 'Lax'
 
 
 GOOGLE_API_KEY='AIzaSyBCIHsOn29OJ5904rIQdufsxJUmWarvJyM'
->>>>>>> c5f785a5e69f4be425f3830ac46b1120e1b9591e
