21ca69bed77ec2680f534d240edfcdc012127010
wedberry <wberry@rollins.edu>
2024-12-01 12:20:22 -0600

    Global shared template to avoid machine-dependent reference errors

--- cc_connect/cc_connect/settings.py
+++ cc_connect/cc_connect/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'cc_connect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # Keep this empty if you rely on app-level templates
+        'DIRS': [BASE_DIR / "templates"],  # Keep this empty if you rely on app-level templates
         'APP_DIRS': True,  # Ensure this is set to True
         'OPTIONS': {
             'context_processors': [
